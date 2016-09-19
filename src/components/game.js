7d1631a32e7f9308808ec78c2515e92ffe3b2623
nick <ncksllvn@gmail.com>
2016-09-19 01:02:25 -0400

    Add a handler for game end

--- src/components/game.js
+++ src/components/game.js
@@ -3,7 +3,7 @@ import Board from './board'
 
 const blackWins = '0-1'
 const whiteWins = '1-0'
-const draw = '1/2-1/2'
+const tie = '1/2-1/2'
 
 export default React.createClass({
 
@@ -13,22 +13,33 @@ export default React.createClass({
 
     componentDidUpdate(){
         if (this.props.board.isGameOver){
-
-            // Feeling lazy so just pop up an alert when the game ends
-            switch (this.props.board.result){
-                case whiteWins:
-                    alert('You win!')
-                    break
-
-                case blackWins:
-                    alert ('Computer wins!')
-                    break
-
-                case tie:
-                    alert('Tie!')
-            }
-
+            this.declareWinner()
         }
+    },
+
+    shouldComponentUpdate(){
+        return !this.props.board.isGameOver
+    },
+
+    declareWinner(){
+
+        var message = ''
+
+        switch (this.props.board.result){
+            case whiteWins:
+                message = 'You win!'
+                break
+
+            case blackWins:
+                message = 'Computer wins!'
+                break
+
+            case tie:
+                message = 'Tie!'
+        }
+
+        // delay the message so that the user has time to see the board first
+        setTimeout(() => alert(message), 3000)
     },
 
     render(){
