a1e258b375da18cfa24721e3201c5ef9824df5f5
Nick Sullivan <ncksllvn@gmail.com>
2019-10-30 21:46:01 -0400

    Remove appEnv check

--- src/index.js
+++ src/index.js
@@ -7,19 +7,15 @@ import {Provider} from 'react-redux'
 import Game from './containers/game'
 import createStore from './stores'
 import {ANALYTICS_RESULTS} from './constants'
-import config from './config'
 
 const store = createStore()
 
-if (config.appEnv == 'dist'){
-    let unsub = store.subscribe(() => {
-        const board = store.getState().board
-        if (board.isGameOver){
-            window.ga('send', 'event', 'game', 'play', 'Result', ANALYTICS_RESULTS[board.result])
-            unsub()
-        }
-    })
-}
+store.subscribe(() => {
+    const board = store.getState().board
+    if (board.isGameOver){
+        window.ga('send', 'event', 'game', 'play', 'Result', ANALYTICS_RESULTS[board.result])
+    }
+})
 
 ReactDOM.render((
     <Provider store={store}>
